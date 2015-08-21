0da7845e936861901998810131d468ba4531a8dc
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2015-08-21 23:04:59 +0200

    Added net load, and bus extractor. Added separate routine for loading pfnet library, prolerly importing the signatures for net and bus functions

--- matlab/Network.m
+++ matlab/Network.m
@@ -3,33 +3,39 @@ classdef Network < handle
   properties
 
     c_net = libpointer;
+    num_buses = 0;
 
   end
 
   methods
 
     function net = Network()
-      net.load_library();
       net.c_net = calllib('libpfnet','NET_new');
     end
 
+    function delete(net)
+      calllib('libpfnet','NET_del',net.c_net);
+    end
+
+    function bus = get_bus(net,i)
+      bus = Bus(calllib('libpfnet','NET_get_bus',net.c_net,i));
+    end
+
+    function load(net,filename)
+      calllib('libpfnet','NET_load',net.c_net,filename);
+    end
+
     function show_components(net)
-      calllib('libpfnet','NET_show_components',net.c_net)
+      calllib('libpfnet','NET_show_components',net.c_net);
+    end
+
+    % Getters and Setters
+    %%%%%%%%%%%%%%%%%%%%%
+
+    function num = get.num_buses(net)
+      num = calllib('libpfnet','NET_get_num_buses',net.c_net);
     end
     
-  end
-
-  methods (Static)
-
-    function load_library()
-      root = getenv('PFNET');
-      header = fullfile(root,'include','pfnet','net.h');
-      lib = fullfile(root,'lib','libpfnet.so');
-      if ~libisloaded('libpfnet')
-         [notfound,warnings] = loadlibrary(lib,header);
-      end
-    end
-        
   end
 
 end
