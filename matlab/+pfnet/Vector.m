c58fd285f557b0ff401a6790b84acf1be6ba50d6
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2015-09-02 16:41:03 +0200

    Figured out how to convert C array returned by calllib to matlab array. Net can now return vector of variable values

---
+++ matlab/+pfnet/Vector.m
@@ -0,0 +1,8 @@
+function vec = Vector(v)
+
+  data = calllib('libpfnet','VEC_get_data',v);
+  size = calllib('libpfnet','VEC_get_size',v);
+
+  setdatatype(data,'doublePtr',1,size);
+  vec = data.Value;
+  
