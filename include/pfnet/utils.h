d7063a955295fcd578d3dd44b9c466221bd74452
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2016-02-09 15:23:38 +0100

    added string trimming util function

---
+++ include/pfnet/utils.h
@@ -0,0 +1,30 @@
+/** @file utils.h
+ *  @brief This file some utiliy functions.
+ *
+ * This file is part of PFNET.
+ *
+ * Copyright (c) 2015-2016, Tomas Tinoco De Rubira.
+ *
+ * PFNET is released under the BSD 2-clause license.
+ */
+
+#ifndef __UTILS_HEADER__
+#define __UTILS_HEADER__
+
+#include <stdio.h>
+#include <ctype.h>
+
+char* trim(char *s) {
+  /* Trims string inplace. */
+  
+  char *ptr;
+  if (!s)
+    return NULL;   // handle NULL string
+  if (!*s)
+    return s;      // handle empty string
+  for (ptr = s + strlen(s) - 1; (ptr >= s) && isspace(*ptr); --ptr);
+  ptr[1] = '\0';
+  return s;
+}
+
+#endif
