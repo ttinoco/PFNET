f6ffadc7ba09b8e6eb52187eaf58a103dd1c6403
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2016-11-25 16:30:00 +0100

    added extra compile args for cython

--- python/setup.py
+++ python/setup.py
@@ -13,18 +13,21 @@ import numpy as np
 import os
 
 libraries = ['pfnet']
+extra_compile_args = []
+
 # raw parser
 if '--no_raw_parser' in sys.argv:
     sys.argv.remove('--no_raw_parser')
+    extra_compile_args.append('-DNO_RAW_PARSER')
 else:
     libraries.append('raw_parser')
 
 # graphviz
 if '--no_graphviz' in sys.argv:
     sys.argv.remove('--no_graphviz')
+    extra_compile_args.append('-DNO_GRAPHVIZ')
 else:
     libraries.append('gvc')
-
 
 setup(name='PFNET',
       version='1.3',
@@ -37,5 +40,6 @@ setup(name='PFNET',
       ext_modules=cythonize([Extension("pfnet.cpfnet",
                                        [os.path.join("pfnet", 'cpfnet.pyx')],
                                        libraries=libraries,
+                                       extra_compile_args=extra_compile_args,
                                        library_dirs=["../lib"],
                                        include_dirs=["../include", np.get_include()])]))
