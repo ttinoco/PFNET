e70ede0df152e1b352284c3328c20d1cd2802ad6
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-03-14 21:55:11 +0100

    debugging

--- python/docs/build.sh
+++ python/docs/build.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 cd ../../
-sudo apt-get install autoconf
-sudo apt-get install automake
+apt-get install autoconf
+apt-get install automake
 ./autogen.sh
 ./configure
 make
-sudo make install
+make install
 cd python
 python setup.py build_ext --inplace
 cd docs
