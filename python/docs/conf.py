08cd558dd41c0bfb5bfd76a6c78e936e15fe9526
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-03-14 20:49:51 +0100

    trying to mock cpfnet

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -22,6 +22,15 @@ import shlex
 sys.path.insert(0, os.path.abspath('../src/'))
 sys.path.insert(0, os.path.abspath('../'))
 
+# mock for cpfnet import
+from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+MOCK_MODULES = ['cpfnet']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
