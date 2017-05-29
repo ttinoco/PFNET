added ac flow lim slow Hessian test, and fixed d2I/dphi2 bug

python/tests/test_constraints.py
@@ -3938,7 +3938,7 @@ class TestConstraints(unittest.TestCase):
                     self.assertLess(error_mk,eps)
                     J_row += 2
 
-            # Jacobian check 1
+            # Jacobian check
             constr.eval(x0,y0)
             f0 = constr.f.copy()
             J0 = constr.J.copy()
@@ -4013,11 +4013,25 @@ class TestConstraints(unittest.TestCase):
                 error = 100.*norm(Hd_exact-Hd_approx)/np.maximum(norm(Hd_exact),tol)
                 self.assertLessEqual(error,EPS)
 
-            # Jacobian check 2
-            if os.environ.get('TEST_SLOW'):
+            # Combined Hessian check
+            coeff = np.random.randn(f0.shape[0])
+            constr.eval(x0,y0)
+            constr.combine_H(coeff,False)
+            H = constr.H_combined.copy()
+            H_manual = 0
+            for i in range(constr.f.size):
+                Hi = constr.get_H_single(i)
+                H_manual = H_manual + coeff[i]*Hi
+            diff = coo_matrix(H_manual-H)
+            self.assertLess(norm(diff.data)/norm(H.data),1e-12)
+
+            # Slow tests
+            if os.environ.get('TEST_SLOW') == '1':
+
+                # Jacobian check
                 h = 1e-12
                 tol = 1e0
-                eps = 5.0 # %
+                eps = 3.0 # %
                 constr.eval(x0,y0)
                 f0 = constr.f.copy()
                 J0 = constr.J.copy()
@@ -4056,6 +4070,84 @@ class TestConstraints(unittest.TestCase):
                             num_bad += 1
                     self.assertLess((100.*num_bad)/net.num_buses,1.) # less then 1 %
 
+                # Sigle Hessian check
+                h = 1e-12
+                tol = 1e0
+                eps = 2.0 # %
+                num_max = 50
+                num_trials = 4
+                counters = [0,0,0]
+                constr_indices = []
+                for t in range(net.num_periods):
+                    for branch in net.branches:
+                        if branch.is_tap_changer() and counters[0] < num_trials:
+                            i = t*net.num_branches*2+2*branch.index
+                            constr_indices += [i,i+1]
+                            counters[0] += 1
+                        elif branch.is_phase_shifter() and counters[1] < num_trials:
+                            i = t*net.num_branches*2+2*branch.index
+                            constr_indices += [i,i+1]
+                            counters[1] += 1
+                        elif counters[2] < num_trials:
+                            i = t*net.num_branches*2+2*branch.index
+                            constr_indices += [i,i+1]
+                            counters[2] += 1
+                self.assertLessEqual(len(constr_indices),24)
+                self.assertEqual(len(constr_indices),len(list(set(constr_indices))))
+                for i in constr_indices:
+                    constr.eval(x0,y0)
+                    g0 = constr.J.tocsr()[i,:].toarray().flatten()
+                    H0 = constr.get_H_single(i)
+                    H0 = (H0 + H0.T) - triu(H0)
+                    def check_H(index):
+                        e = np.zeros(net.num_vars+constr.num_extra_vars)
+                        e[index] = 1.
+                        x = x0 + h*e[:net.num_vars]
+                        y = y0 + h*e[net.num_vars:]
+                        constr.eval(x,y)
+                        g1 = constr.J.tocsr()[i,:].toarray().flatten()
+                        He_exact = H0*e
+                        He_approx = (g1-g0)/h
+                        error = 100.*norm(He_exact-He_approx)/np.maximum(norm(He_exact),tol)
+                        try:
+                            self.assertLessEqual(error,eps)
+                            return True
+                        except AssertionError:
+                            return False
+                    for t in range(net.num_periods):
+                        counter = 0
+                        for branch in net.branches:
+                            if branch.is_tap_changer():
+                                self.assertTrue(check_H(branch.index_ratio[t]))
+                                counter += 1
+                                if counter >= num_max:
+                                    break
+                        counter = 0
+                        for branch in net.branches:
+                            if branch.is_phase_shifter():
+                                self.assertTrue(check_H(branch.index_phase[t]))
+                                counter += 1
+                                if counter >= num_max:
+                                    break
+                        num_bad = 0
+                        counter = 0
+                        for bus in net.buses:
+                            if not check_H(bus.index_v_mag[t]):
+                                num_bad += 1
+                            counter += 1
+                            if counter >= num_max:
+                                break
+                        self.assertLess((100.*num_bad)/min([net.num_buses,num_max]),1.) # less then 1 %
+                        num_bad = 0
+                        counter = 0
+                        for bus in net.buses:
+                            if not check_H(bus.index_v_ang[t]):
+                                num_bad += 1
+                            counter += 1
+                            if counter >= num_max:
+                                break
+                        self.assertLess((100.*num_bad)/min([net.num_buses,num_max]),1.) # less then 1 %
+                    
     def test_constr_DUMMY(self):
 
         # Multiperiod