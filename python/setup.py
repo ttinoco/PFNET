7bf0a7b4e0fe7a0585b70c9dfa7eb578db362768
Adam Wigington <ajwigington@gmail.com>
2017-06-05 16:32:19 -0700

    changes to rpath dynamic library links to and from libpfnet including osx specific loader_path when building wheel for distribution

--- python/setup.py
+++ python/setup.py
@@ -6,9 +6,20 @@
 # PFNET is released under the BSD 2-clause license. #
 #***************************************************#
 
+import sys
 import numpy as np
 from Cython.Build import cythonize
 from setuptools import setup, Extension
+
+package_data = {}
+extra_link_args = []
+
+# need to check if building distributable wheel and make sure to package libpfnet.*
+if 'bdist_wheel' in sys.argv:
+    package_data={'pfnet': ["libpfnet.*"]}
+    # if on OSX add loader_path to rpath, so libpfnet.* is located
+    if sys.platform.lower() == 'darwin':
+        extra_link_args.append("-Wl,-path,@loader_path/")
 
 setup(name='PFNET',
       version='1.3.0',
@@ -17,10 +28,14 @@ setup(name='PFNET',
       author='Tomas Tinoco De Rubira',
       author_email='ttinoco5687@gmail.com',
       url='https://github.com/ttinoco/PFNET/python',
+      include_package_data=True,
+      package_data=package_data,
       packages=['pfnet',
                 'pfnet.parsers',
                 'pfnet.functions',
                 'pfnet.constraints'],
       ext_modules=cythonize([Extension(name="pfnet.cpfnet", 
                                        sources=["./pfnet/cpfnet.pyx"],
-                                       include_dirs=[np.get_include()])]))
+                                       include_dirs=[np.get_include()],
+                                       extra_link_args=extra_link_args
+                                       )]))
