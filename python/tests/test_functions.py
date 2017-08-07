0e061182e8fa050d456abfa6686567acf3623f30
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-08-06 18:52:19 -0700

    updated test_functions fin diff h to try to make travis happy

--- python/tests/test_functions.py
+++ python/tests/test_functions.py
@@ -29,7 +29,7 @@ class TestFunctions(unittest.TestCase):
     def test_func_REG_VMAG(self):
 
         # Constants
-        h = 1e-9
+        h = 1e-8
 
         # Multiperiod
         for case in test_cases.CASES:
@@ -940,7 +940,7 @@ class TestFunctions(unittest.TestCase):
     def test_func_SP_CONTROLS(self):
 
         # Constants
-        h = 1e-9
+        h = 1e-10
 
         # Single period
         for case in test_cases.CASES:
