7f2a5a84d55d4a3163848942e515284138cdc072
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-09-20 11:33:17 -0700

    removed sample build-cmake files, added cmakelists to tarball, start build_lib.bat

--- build-cmake.sh
+++
@@ -1,3 +0,0 @@
-( cd ./cmake-build
-  cmake -DCMAKE_INSTALL_PREFIX=../cmake-install -DRAW_PARSER_SOURCE_DIR=/home/adam/projects/romcon/raw-parser/ -G"Unix Makefiles" -DPFNET_GRAPHVIZ=ON ..
-)
---
+++ python/build_lib.bat
@@ -0,0 +1,12 @@
+IF NOT EXIST "lib\pfnet" (
+  cd lib
+  7z x pfnet*.tar.gz
+  move pfnet*.tar pfnet.tar
+  7z x pfnet.tar
+  del pfnet.tar
+  for /d %%G in ("pfnet*") do move "%%~G" pfnet
+  cd pfnet
+  cmake -DCMAKE_INSTALL_PREFIX=.\build -G"MinGW Makefiles" -DPFNET_GRAPHVIZ=OFF -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DRAW_PARSER_SOURCE_DIR=%RAW_PARSER% .
+  mingw32-make -j
+  mingw32-make install
+)
