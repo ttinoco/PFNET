002c9bd4cd24c3e07ee91ac667cc66c59f2f0696
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-09-20 14:04:58 -0700

    debugging windows

--- python/build_lib.bat
+++ python/build_lib.bat
@@ -6,7 +6,7 @@ IF NOT EXIST "lib\pfnet" (
   del pfnet.tar
   for /d %%G in ("pfnet*") do move "%%~G" pfnet
   cd pfnet
-  cmake -DCMAKE_INSTALL_PREFIX=.\build -G"MinGW Makefiles" -DPFNET_GRAPHVIZ=OFF -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DRAW_PARSER_SOURCE_DIR=%RAW_PARSER% .
+  cmake -DCMAKE_INSTALL_PREFIX=.\build -G"MinGW Makefiles" -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE .
   mingw32-make -j
   mingw32-make install
   copy build\lib\* ..\..\pfnet\
