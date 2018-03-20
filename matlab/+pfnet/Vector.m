66bbce89a894cfbe7a03ccb4d60176f49a730037
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2018-03-19 22:09:06 -0700

    matlab gone

--- matlab/+pfnet/Vector.m
+++
@@ -1,8 +0,0 @@
-function vec = Vector(v)
-
-  data = calllib('libpfnet','VEC_get_data',v);
-  size = calllib('libpfnet','VEC_get_size',v);
-
-  setdatatype(data,'doublePtr',1,size);
-  vec = data.Value;
-end
