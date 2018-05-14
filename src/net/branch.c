f2d53a7f69eb1271a0e72edec6c604b631388bd2
Adam Wigington <ajwigington@gmail.com>
2018-05-14 16:55:24 -0700

    added guards for gcc7 default support for -Werror misleading-identations

--- src/net/branch.c
+++ src/net/branch.c
@@ -1622,10 +1622,11 @@ void BRANCH_set_pos_ratio_v_sens(Branch* br, BOOL flag) {
 }
 
 void BRANCH_set_outage(Branch* br, BOOL outage) {
-  if (br)
+  if (br) {
     if (br->outage != outage)
       NET_inc_state_tag(br->net);
     br->outage = outage;
+  }
 }
 
 void BRANCH_set_phase(Branch* br, REAL phase, int t) {
--- src/net/gen.c
+++ src/net/gen.c
@@ -893,10 +893,11 @@ void GEN_set_reg_bus(Gen* gen, Bus* reg_bus) {
 }
 
 void GEN_set_outage(Gen* gen, BOOL outage) {
-  if (gen)
+  if (gen) {
     if (gen->outage != outage)
       NET_inc_state_tag(gen->net);
     gen->outage = outage;
+  }
 }
 
 void GEN_set_index(Gen* gen, int index) {
