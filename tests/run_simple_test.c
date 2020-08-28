5530655a7f2b01a3e3fe4b538d1e03a0298ce493
Adam Wigington <awigington@epri.com>
2020-08-28 08:33:33 -0700

    Updating CMakeLists to simplify the tests that are built and add the simple tests

--- tests/run_simple_test.c
+++ tests/run_simple_test.c
@@ -14,22 +14,26 @@ int main(int argc, char **argv) {
 
   // Get case
   test_case = argv[1];
-  printf("starting test to build parse and build net form %s", test_case);
+  printf("starting test to build parse and build net from %s\n", test_case);
 
   // Run tests
   Parser* parser;
   Net* net;
 
-  printf("test_net_load ... ");
+  printf("Parsing network ... \n");
 
   parser = PARSER_new_for_file(test_case);
+
+  printf("Loading network from parser ... \n");
   net = PARSER_parse(parser, test_case, 1);
 
   NET_check(net, 1);
   NET_show_components(net, 2);
 
+  printf("Cleaning up ... \n");
+
   NET_del(net);
   PARSER_del(parser);
 
-  printf("complete");
+  printf("complete!\n");
 }
